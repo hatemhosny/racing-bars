0874e4a88ea271b6f5ba41e27c20f56f9311f423
Hatem Hosny <hatemhosny@gmail.com>
2020-06-05 18:40:35 +0200

    clean-up

--- demo/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { createStore, actions } from '../../src/lib/store';
-import { rootReducer } from '../../src/lib/store/reducer';
-
-const store = createStore(rootReducer);
-store.dispatch(actions.options.optionsLoaded({ startDate: '1980-07-01', endDate: '2010-12-31' }));
-
-console.log(store.getState());
