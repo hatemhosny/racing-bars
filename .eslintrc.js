a25f00001d44162b9a3ef4a6207e51c9baf674e1
Hatem Hosny <hatemhosny@gmail.com>
2020-11-29 13:54:48 +0200

    wip linting from cli fails

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.eslint.json',
+    project: ['./tsconfig.json', './src/angular/tsconfig.json'],
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'jest', 'jsdoc'],
