c026f120e320ed0a4b3ce3a1ac8680e1834b3543
Hatem Hosny <hatemhosny@gmail.com>
2023-12-29 13:21:39 +0200

    docs: improve show code

--- website/src/components/RacingBars/index.tsx
+++ website/src/components/RacingBars/index.tsx
@@ -1,8 +1,9 @@
 import React, { Suspense, lazy } from 'react';
-import type { Options, Props } from '../../../../src/index';
+import type { Props } from '../../../../src/index';
 import ShowCode from '../ShowCode';
 import styles from './styles.module.css';
 import BrowserOnly from '@docusaurus/BrowserOnly';
+import { getFrameworkCode } from '@site/src/helpers/get-framework-code';
 
 export default function RacingBars(
   props: Props & {
@@ -11,67 +12,8 @@ export default function RacingBars(
     showCode?: 'open' | 'closed' | false;
   },
 ): JSX.Element {
-  const stringify = (obj: Partial<Options>) => JSON.stringify(obj, null, 2);
   const { className, style, showCode, ...options } = props;
-  const dataUrl = options.dataUrl || '';
-
-  const getOptionsCode = (options: any, lang: 'js' | 'ts' | 'react' | 'vue' | 'svelte') => {
-    const { data, dataUrl, ...codeOptions } = options;
-    if (lang === 'ts') {
-      return `\nconst options: Options = ${stringify(codeOptions)};`;
-    }
-    if (lang === 'react') {
-      return `const options = ${stringify({ dataUrl, ...codeOptions })};`;
-    }
-    if (lang === 'vue') {
-      return `\nconst options = ${stringify({ dataUrl, ...codeOptions })};`;
-    }
-    return `\nconst options = ${stringify(codeOptions)};`;
-  };
-
-  const jsCode = `
-import { race } from "racing-bars";
-${getOptionsCode(options, 'js')}
-race("${dataUrl}", "#race", options);
-`.trimStart();
-
-  const tsCode = `
-import { race, type Options } from "racing-bars";
-${getOptionsCode(options, 'ts')}
-race("${dataUrl}", "#race", options);
-`.trimStart();
-
-  const reactCode = `
-import RacingBars from "racing-bars/react";
-
-export default function App() {
-  ${getOptionsCode(options, 'react')}
-  return (<RacingBars {...options}>Loading...</RacingBars>);
-}
-`.trimStart();
-
-  const vueCode = `
-<script setup>
-import RacingBars from "racing-bars/vue";
-${getOptionsCode(options, 'vue')}
-</script>
-<template>
-  <RacingBars v-bind="options">Loading...</RacingBars>
-</template>
-`.trimStart();
-
-  const svelteCode = `
-<script>
-import { onMount } from "svelte";
-import { race } from "racing-bars";
-${getOptionsCode(options, 'svelte')}
-onMount(() => {
-  race("${dataUrl}", "#race", options);
-});
-</script>
-
-<div id="race">Loading...</div>
-`.trimStart();
+  const { jsCode, tsCode, reactCode, vueCode, svelteCode } = getFrameworkCode(options);
 
   const RacingBarsReact: React.ComponentType<Props & { children?: React.ReactNode }> = lazy(
     // @ts-ignore
