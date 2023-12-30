937c7048a7e4a56c3fd3bd5128d2b4b3abb31cd6
Hatem Hosny <hatemhosny@gmail.com>
2023-12-30 03:07:29 +0200

    jest works

--- src/lib/styles/__tests__/style-inject.spec.ts
+++ src/lib/styles/__tests__/style-inject.spec.ts
@@ -8,11 +8,12 @@ describe('style inject', () => {
     head.innerHTML = '';
     testElement = document.createElement('title');
     head.appendChild(testElement);
+    document.body.innerHTML = '<div id="race"></div>';
   });
 
   test('should return empty string, if no css', () => {
     expect(
-      styleInject(document.querySelector('.race')!, 'light', undefined, '', {
+      styleInject(document.querySelector('#race')!, 'light', undefined, '', {
         light: '',
         dark: '',
       }),
@@ -21,23 +22,24 @@ describe('style inject', () => {
   });
 
   test('should return empty string, if no selector', () => {
-    expect(styleInject(document.querySelector('')!, 'light')).toBe('');
+    expect(styleInject(null!, 'light')).toBe('');
     expect(head.querySelector('style')).toBeNull();
   });
 
   test('should insert style element to document head', () => {
-    styleInject(document.querySelector('.race')!, 'light');
+    styleInject(document.querySelector('#race')!, 'light');
     expect(head.querySelector('style')).toBeDefined();
   });
 
   test('should return id of inerted style element', () => {
-    const styleId = styleInject(document.querySelector('.race')!, 'light');
+    const styleId = styleInject(document.querySelector('#race')!, 'light');
     expect(typeof styleId).toBe('string');
+    console.log(styleId);
     expect(styleId.startsWith('styles')).toBe(true);
   });
 
   test('should insert style element at the top of document head', () => {
-    const styleId = styleInject(document.querySelector('.race')!, 'light');
+    const styleId = styleInject(document.querySelector('#race')!, 'light');
     const elements = head.childNodes;
     const insertedStyle = head.querySelector('#' + styleId);
     expect(elements[0]).toBe(insertedStyle);
@@ -46,14 +48,14 @@ describe('style inject', () => {
 
   test('should insert style element in document head if no other elements', () => {
     head.innerHTML = '';
-    const styleId = styleInject(document.querySelector('.race')!, 'light');
+    const styleId = styleInject(document.querySelector('#race')!, 'light');
     const elements = head.childNodes;
     const insertedStyle = head.querySelector('#' + styleId);
     expect(elements[0]).toBe(insertedStyle);
   });
 
   test('can be configured to insert style element at the bottom of document head', () => {
-    const styleId = styleInject(document.querySelector('.race')!, 'light', 'bottom');
+    const styleId = styleInject(document.querySelector('#race')!, 'light', 'bottom');
     const elements = head.childNodes;
     const insertedStyle = head.querySelector('#' + styleId);
     expect(elements[0]).toBe(testElement);
@@ -66,7 +68,7 @@ describe('style inject', () => {
       light: '__selector__ div { color: white; }',
       dark: '__selector__ div { color: black; }',
     };
-    const selector = '.race';
+    const selector = '#race';
     const css = (styles + themes.light).split('__selector__').join(selector);
     const styleId = styleInject(document.querySelector(selector)!, 'light', 'top', styles, themes);
     const insertedStyle = head.querySelector('#' + styleId);
@@ -80,7 +82,7 @@ describe('style inject', () => {
       light: '__selector__ div { color: white; }',
       dark: '__selector__ div { color: black; }',
     };
-    const selector = '.race';
+    const selector = '#race';
     const css = (styles + themes.dark).split('__selector__').join(selector);
     const styleId = styleInject(document.querySelector(selector)!, 'dark', 'top', styles, themes);
     const insertedStyle = head.querySelector('#' + styleId);
