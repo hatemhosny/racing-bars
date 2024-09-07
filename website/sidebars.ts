7991d4cdf24491e72eb9cec01e2dbea9e7158002
Hatem Hosny <hatemhosny@gmail.com>
2024-09-07 03:04:25 +0300

    fix

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -6,9 +6,9 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
  - render a sidebar for each doc of that group
  - provide next/previous navigation
 
- The sidebars can be generated from the filesystem, or explicitly defined here.
+  The sidebars can be generated from the filesystem, or explicitly defined here.
 
- Create as many sidebars as you want.
+  Create as many sidebars as you want.
  */
 const sidebars: SidebarsConfig = {
   docsSidebar: [
